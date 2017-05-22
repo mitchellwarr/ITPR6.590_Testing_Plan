0c2fcb8b6a001ae2fe95cd056c7c01f7aa87e26b
Sarah Grace Anderson <ANDERS21@soc.local>
2017-05-22 16:11:13 +1200

    driver tests

--- driverApp/src/driver_test.go
+++ driverApp/src/driver_test.go
@@ -2,32 +2,85 @@ package main
 
 import (
 	"testing"
+	"fmt"
 )
 
+// 
 func TestDriverInCity(t *testing.T) {
+	outsideCity := &node{id: 0}
+	mayfair := &node{id: 1}
+	akina := &node{id: 2}
+	stortfordLodge := &node{id: 3}
+	mahora := &node{id: 4}
 
+	driverOutsideCity := driver{}
+	driverOutsideCity.location = outsideCity
+
+	driverMayfair := driver{}
+	driverMayfair.location = mayfair
+
+	driverAkina := driver{}
+	driverAkina.location = akina
+
+	driverStortfordLodge := driver{}
+	driverStortfordLodge.location = stortfordLodge
+
+	driverMahora := driver{}
+	driverMahora.location = mahora
+
+	boolDriverOutsideCity := driverOutsideCity.driverInCity();
+	boolDriverMayfair := driverMayfair.driverInCity();
+	boolDriverAkina := driverAkina.driverInCity();
+	boolDriverStortfordLodge := driverStortfordLodge.driverInCity();
+	boolDriverMahora := driverMahora.driverInCity();
+
+	AssertFalse(t, "OutsideCity driver should not be inside city", boolDriverOutsideCity)
+	AssertTrue(t, "Mayfair driver should be inside city", boolDriverMayfair)
+	AssertTrue(t, "Akina driver should be inside city", boolDriverAkina)
+	AssertTrue(t, "StortfordLodge driver should be inside city", boolDriverStortfordLodge)
+	AssertTrue(t, "Mahora driver should be inside city", boolDriverMahora)
 }
 
-func TestVisitMessage(){
-
+func AssertEqual(t *testing.T, message string, item1, item2 interface{} ) {
+	if item1 != item2 {
+		t.Error("FAILED:", message, "- item1:", item1, "item2:", item2)
+	} else {
+		fmt.Println("PASS:", message)
+	}
 }
 
-func TestStart(){
-
+func AssertTrue(t *testing.T, message string, item bool ) {
+	if item {
+		fmt.Println("PASS:", message)
+	} else {
+		t.Error("FAILED:", message, "- item:", item)
+	}
 }
 
-func TestMove(){
-
+func AssertFalse(t *testing.T, message string, item bool ) {
+	AssertTrue(t, message, !item)
 }
 
-func TestPickNeightbour(){
+// func TestVisitMessage(){
 
-}
+// }
 
-func TestTryMeetJohn(){
+// func TestStart(){
 
-}
+// }
 
-func TestChechExit(){
+// func TestMove(){
+
+// }
+
+// func TestPickNeightbour(){
+
+// }
+
+// func TestTryMeetJohn(){
+
+// }
+
+// func TestChechExit(){
 	
-}
+// }
